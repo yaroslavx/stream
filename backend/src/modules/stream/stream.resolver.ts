97ff5fd372fe449441fae8f431bc716b8bf0d7ea
yaroslavx <yakupov.yaroslav@icloud.com>
2025-07-02 20:59:42 +0300

    MAINTAIN: IMplemented stream filters for fetching

--- backend/src/modules/stream/stream.resolver.ts
+++ backend/src/modules/stream/stream.resolver.ts
@@ -1,13 +1,19 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { StreamService } from "./stream.service";
 import { StreamModel } from "@/src/modules/stream/models/stream.model";
+import { FiltersInput } from "./inputs/filters.input";
 
 @Resolver("Stream")
 export class StreamResolver {
   public constructor(private readonly streamService: StreamService) {}
 
   @Query(() => [StreamModel], { name: "findAllStreams" })
-  public async findAll() {
-    return this.streamService.findAll();
+  public async findAll(@Args("filters") input: FiltersInput) {
+    return this.streamService.findAll(input);
+  }
+
+  @Query(() => [StreamModel], { name: "findRandomStreams" })
+  public async findRandom() {
+    return this.streamService.findRandom();
   }
 }
--- backend/src/modules/stream/stream.service.ts
+++ backend/src/modules/stream/stream.service.ts
@@ -1,24 +1,87 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "@/src/core/prisma/prisma.service";
+import { FiltersInput } from "./inputs/filters.input";
+import type { Prisma } from "@/prisma/generated";
 
 @Injectable()
 export class StreamService {
   public constructor(private readonly prismaService: PrismaService) {}
 
-  public async findAll() {
+  public async findAll(input: FiltersInput = {}) {
+    const { take, skip, searchTerm } = input;
+
+    const whereClause = searchTerm
+      ? this.findBySearchTermFilter(searchTerm)
+      : undefined;
+
+    const streams = await this.prismaService.stream.findMany({
+      take: take ?? 12,
+      skip: skip ?? 0,
+      where: {
+        user: {
+          isDeactivated: false,
+        },
+        ...whereClause,
+      },
+      include: {
+        user: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return streams;
+  }
+
+  public async findRandom() {
+    const total = await this.prismaService.stream.count({
+      where: {
+        user: {
+          isDeactivated: false,
+        },
+      },
+    });
+
+    const randomIndexes = new Set<number>();
+
+    while (randomIndexes.size < (total < 4 ? total : 4)) {
+      const randomIndex = Math.floor(Math.random() * total);
+      randomIndexes.add(randomIndex);
+    }
+
     const streams = await this.prismaService.stream.findMany({
       where: {
         user: {
           isDeactivated: false,
         },
       },
-      include: {
-        user: {
-          where: { isDeactivated: false },
-        },
-      },
+      include: { user: true },
+      take: total,
+      skip: 0,
     });
 
-    return streams;
+    return Array.from(randomIndexes).map((index) => streams[index]);
+  }
+
+  private findBySearchTermFilter(searchTerm: string): Prisma.StreamWhereInput {
+    return {
+      OR: [
+        {
+          title: {
+            contains: searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          user: {
+            username: {
+              contains: searchTerm,
+              mode: "insensitive",
+            },
+          },
+        },
+      ],
+    };
   }
 }
