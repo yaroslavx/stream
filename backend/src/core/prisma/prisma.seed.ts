56e499d58f8e38e5c825c0499cde308e821b30f9
yaroslavx <yakupov.yaroslav@icloud.com>
2025-07-12 11:25:48 +0300

    MAINTAIN: Created db seed

--- backend/src/core/prisma/prisma.seed.ts
+++ backend/src/core/prisma/prisma.seed.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Logger } from "@nestjs/common";
 import { PrismaClient } from "../../../prisma/generated";
+import { CATEGORIES } from "./data/categories.data";
+import { STREAMS } from "./data/streams.data";
+import { USERNAMES } from "./data/users.data";
+import { hash } from "argon2";
 
 const prisma = new PrismaClient();
 
@@ -13,6 +17,95 @@ async function main() {
       prisma.stream.deleteMany(),
       prisma.category.deleteMany(),
     ]);
+
+    const categoriesData = CATEGORIES;
+
+    await prisma.category.createMany({
+      data: categoriesData,
+    });
+
+    Logger.log("Категории созданы");
+
+    const categories = await prisma.category.findMany();
+
+    const categoriesBySlug = Object.fromEntries(
+      categories.map((category) => [category.slug, category]),
+    );
+
+    const streamsData: Record<string, string[]> = STREAMS;
+
+    const usernamesData = USERNAMES;
+
+    await prisma.$transaction(async (tx) => {
+      for (const username of usernamesData) {
+        const randomCategory =
+          categoriesBySlug[
+            Object.keys(categoriesBySlug)[
+              Math.floor(Math.random() * Object.keys(categoriesBySlug).length)
+            ]
+          ];
+
+        const userExists = await tx.user.findUnique({
+          where: { username },
+        });
+
+        if (!userExists) {
+          const createdUser = await tx.user.create({
+            data: {
+              email: `${username}@gmail.com`,
+              password: await hash("12345678"),
+              username,
+              displayName: username,
+              avatar: `/channels/${username}.webp`,
+              isEmailVerified: true,
+              socialLinks: {
+                createMany: {
+                  data: [
+                    {
+                      title: "Telegram",
+                      url: `https://t.me/${username}`,
+                      position: 1,
+                    },
+                    {
+                      title: "YouTube",
+                      url: `https://youtube.com/@${username}`,
+                      position: 2,
+                    },
+                  ],
+                },
+              },
+            },
+          });
+
+          const randomTitles = streamsData[randomCategory.slug];
+          const randomTitle =
+            randomTitles[Math.floor(Math.random() * randomTitles.length)];
+
+          await tx.stream.create({
+            data: {
+              title: randomTitle,
+              thumbnailUrl: `/streams/${createdUser.username}.webp`,
+              user: {
+                connect: {
+                  id: createdUser.id,
+                },
+              },
+              category: {
+                connect: {
+                  id: randomCategory.id,
+                },
+              },
+            },
+          });
+
+          Logger.log(
+            `Пользователь "${createdUser.username}" и его стри успешно созданы`,
+          );
+        }
+      }
+    });
+
+    Logger.log("Заполнение БД завершено");
   } catch (error) {
     Logger.log(error);
     throw new BadRequestException("Ошибки при заполнении БД");
