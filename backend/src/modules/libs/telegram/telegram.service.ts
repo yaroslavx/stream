8975bd079d56363d69a97a6fa87c5945646fb854
yaroslavx <yakupov.yaroslav@icloud.com>
2025-07-26 07:21:19 +0300

    MAINTAIN: Started telegraf service

--- backend/src/modules/libs/telegram/telegram.service.ts
+++ backend/src/modules/libs/telegram/telegram.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Update } from "nestjs-telegraf";
+import { Telegraf } from "telegraf";
+import { PrismaService } from "@/src/core/prisma/prisma.service";
+import { ConfigService } from "@nestjs/config";
 
+@Update()
 @Injectable()
-export class TelegramService {}
+export class TelegramService extends Telegraf {
+  private readonly _token: string;
+
+  public constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+  ) {
+    super(configService.getOrThrow<string>("TELEGRAM_BOT_TOKEN"));
+    this._token = this.configService.getOrThrow<string>("TELEGRAM_BOT_TOKEN");
+  }
+}
