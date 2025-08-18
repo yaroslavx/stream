489c3413058b3d0e7de5e3ff8b84af50ffb485d1
yaroslavx <yakupov.yaroslav@icloud.com>
2025-08-18 21:08:02 +0300

    MAINTAIN: Completed transactions

--- backend/src/modules/sponsorship/transaction/transaction.resolver.ts
+++ backend/src/modules/sponsorship/transaction/transaction.resolver.ts
@@ -1,7 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
-import { TransactionService } from './transaction.service';
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { TransactionService } from "./transaction.service";
+import { TransactionModel } from "@/src/modules/sponsorship/transaction/models/transaction.model";
+import { Authorization } from "@/src/shared/decorators/auth.decorator";
+import { Authorized } from "@/src/shared/decorators/authorized.decorator";
+import { User } from "@prisma/generated";
+import { MakePaymentModel } from "@/src/modules/sponsorship/transaction/models/make-payment.model";
 
-@Resolver('Transaction')
+@Resolver("Transaction")
 export class TransactionResolver {
-  constructor(private readonly transactionService: TransactionService) {}
+  public constructor(private readonly transactionService: TransactionService) {}
+
+  @Authorization()
+  @Query(() => [TransactionModel], { name: "findMyTransactions" })
+  public async findMyTransaction(@Authorized() user: User) {
+    return this.transactionService.findMyTransactions(user);
+  }
+
+  @Authorization()
+  @Mutation(() => MakePaymentModel, { name: "makePayment" })
+  public async makePayment(
+    @Authorized() user: User,
+    @Args("planId") planId: string,
+  ) {
+    return this.transactionService.makePayment(user, planId);
+  }
 }
