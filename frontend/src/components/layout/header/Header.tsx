33e6c67f6cce0ca13d70b92a5f4b3cd44c9c3a46
yaroslavx <yakupov.yaroslav@icloud.com>
2025-11-16 10:08:41 +0300

    MAINTAIN: Created header search

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "@/components/layout/header/Logo";
+import { Search } from "@/components/layout/header/Search";
 
 export function Header() {
   return (
     <header className="flex h-full items-center gap-x-4 border-b border-border bg-card p-4">
       <Logo />
+      <Search />
     </header>
   );
 }
---
+++ frontend/src/components/layout/header/Search.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/router";
+import { useTranslations } from "next-intl";
+import { FormEvent, useState } from "react";
+import { Button } from "@/components/ui/common/Button";
+import { Input } from "@/components/ui/common/Input";
+
+export function Search() {
+  const t = useTranslations("layout.search");
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (searchTerm.trim()) {
+      router.push(`/streams?searchTerm=${searchTerm}`);
+    } else {
+      router.push("/streams");
+    }
+  }
+
+  return (
+    <div className="ml-auto hidden lg:block">
+      <form className="relative flex items-center" onSubmit={onSubmit}>
+        <Input
+          placeholder={t("placeholder")}
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full rounded-full pl-4 pr-10 lg:w-[400px]"
+        />
+        <Button className="absolute right-0.5 h-9 type-submit">
+          <SearchIcon className="absolute size-[18px]" />
+        </Button>
+      </form>
+    </div>
+  );
+}
