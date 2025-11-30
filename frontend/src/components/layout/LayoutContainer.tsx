52ab50eda9d89637986268b164fc8f90dc1a0037
yaroslavx <yakupov.yaroslav@icloud.com>
2025-11-30 16:04:58 +0300

    MAINTAIN: Modified layout container

--- frontend/src/components/layout/LayoutContainer.tsx
+++ frontend/src/components/layout/LayoutContainer.tsx
@@ -1,5 +1,31 @@
-import { PropsWithChildren } from "react";
+"useClient";
+
+import { PropsWithChildren, useEffect } from "react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { useSidebar } from "@/hooks/useSidebar";
+import { cn } from "@/utils/tw-merge";
 
 export function LayoutContainer({ children }: PropsWithChildren<unknown>) {
-  return <main className="mt-[75px] flex-1 px-8">{children}</main>;
+  const isMobile = useMediaQuery("(max-width: 1024px)");
+
+  const { isCollapsed, collapse, open } = useSidebar();
+
+  useEffect(() => {
+    if (!isMobile && !isCollapsed && !isMobile) {
+      open();
+    } else if (!isMobile && isCollapsed) {
+      collapse();
+    }
+  }, [isMobile, isCollapsed, collapse, open]);
+
+  return (
+    <main
+      className={cn(
+        "mt-[75px] flex-1 px-8",
+        isCollapsed ? "ml-16" : "ml-16 lg:ml-64",
+      )}
+    >
+      {children}
+    </main>
+  );
 }
