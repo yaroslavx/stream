30f91fbfddc4d4990c6ad693a90e2206a212d55d
yaroslavx <yakupov.yaroslav@icloud.com>
2026-01-18 21:24:10 +0300

    MAINTAIN: Created change password request

---
+++ frontend/src/schemas/user/change-password.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const changePasswordSchema = z.object({
+  oldPassword: z.string().min(8),
+  newPassword: z.string().min(8),
+});
+
+export type TypeChangePasswordSchema = z.infer<typeof changePasswordSchema>;
