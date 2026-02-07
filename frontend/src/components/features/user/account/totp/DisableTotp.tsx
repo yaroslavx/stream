c11b1be4052b7a976afd4724576f961939883678
yaroslavx <yakupov.yaroslav@icloud.com>
2026-02-07 12:44:32 +0300

    MAINTAIN: Completed totp management

--- frontend/src/components/features/user/account/totp/DisableTotp.tsx
+++ frontend/src/components/features/user/account/totp/DisableTotp.tsx
@@ -1,3 +1,26 @@
+import { Button } from "@/components/ui/common/Button";
+import { ConfirmModal } from "@/components/ui/elements/ConfirmModal";
+import { useDisableTotpMutation } from "@/graphql/generated/output";
+import { useCurrent } from "@/hooks/useCurrent";
+import { useTranslations } from "next-intl";
+import { toast } from "sonner";
+
 export function DisableTotp() {
-  return <div></div>;
+  const t = useTranslations("dashboard.settings.account.twoFactor.disable");
+
+  const { refetch } = useCurrent();
+
+  const [disableTotp, { loading: isDisableTotpLoading }] = useDisableTotpMutation({
+    onCompleted: () => {
+      refetch();
+      toast.success(t("successMessage"));
+    },
+    onError: () => {
+      toast.error(t("errorMessage"));
+    },
+  });
+
+  return <ConfirmModal heading={t("heading")} message={t("message")} onConfirm={() => disableTotp()}>
+    <Button variant="secondary" disabled={isDisableTotpLoading}>{t("trigger")}</Button>
+  </ConfirmModal>;
 }
